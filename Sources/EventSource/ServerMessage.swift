4119d622e459c91a39ec097c38ca2007946de276
Firdavs Khaydarov <recouse@recouse.email>
2024-01-26 12:26:35 +0200

    Added trimming whitespaces for keys and values of the message
    Attempt to fix #3

--- Sources/EventSource/ServerMessage.swift
+++ Sources/EventSource/ServerMessage.swift
@@ -53,8 +53,8 @@ public struct ServerMessage {
         
         for row in rows {
             let keyValue = row.split(separator: MessageParser.semicolon, maxSplits: 1)
-            let key = keyValue[0].utf8String
-            let value = keyValue[1].utf8String
+            let key = keyValue[0].utf8String.trimmingCharacters(in: .whitespaces)
+            let value = keyValue[1].utf8String.trimmingCharacters(in: .whitespaces)
             
             switch key {
             case "id":
