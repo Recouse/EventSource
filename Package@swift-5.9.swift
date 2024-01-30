cf1de3cefdcee3aacdf6f3f0b717cef07d616bd0
Firdavs Khaydarov <recouse@recouse.email>
2024-01-30 14:28:59 +0200

    Updated swift-async-algorithms to version 1.0.0

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["EventSource"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "0.1.0")
+         .package(url: "https://github.com/apple/swift-async-algorithms.git", from: "1.0.0")
     ],
     targets: [
         .target(
